3729647426afb7a4299e53123932fa6a6c2f6deb
Madison <mkelly1081@gmail.com>
2022-01-08 08:35:01 -0600

    Heroku deployment configurations

--- index.js
+++ index.js
@@ -18,10 +18,17 @@ mongoose
     .then(() => console.log("MongoDB Connected"))
     .catch((err)=> console.log(err));
 
-    app.use("/api/pins", pinRoute);
-    app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+app.use("/api/users", userRoute);
 
 
-app.listen(8080, () => {
-    console.log("Backend server is running");
+// For Heorku Deployment
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "/client/build")))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")))
+}
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Backend server is running ${PORT} on ${process.env.NODE_ENV}`);
 });
